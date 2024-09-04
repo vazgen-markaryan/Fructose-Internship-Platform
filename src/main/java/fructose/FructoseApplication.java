8ee874104c0a5acdb3afc301872d4011d61fab20
Vazgen Markaryan <vazgenmarkaryan21@.com>
2024-09-04 16:22:00 -0400

    Initialisation du Spring + React

---
+++ src/main/java/fructose/FructoseApplication.java
@@ -0,0 +1,18 @@
+package fructose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FructoseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FructoseApplication.class, args);
+		
+//		TO RUN THE APPLICATION:
+//		1. Run this file
+//		2. Go to CMD, Change Directory into front-end/fructose and run "npm start"
+//		3. Go to browser and type "localhost:3000"
+	
+//		package.json a été modifié. Le proxy sur la ligne 5 est ajouté pour le future redirect
+	}
+}
---
+++ src/test/java/fructose/FructoseApplicationTests.java
@@ -0,0 +1,13 @@
+package fructose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FructoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
