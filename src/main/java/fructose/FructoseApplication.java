7af563e0efe0939583eedfe2251160172cdb64e5
Vazgen Markaryan <vazgenmarkaryan21@.com>
2024-09-11 12:09:36 -0400

    POSTGRESQL EST CONFIGURE

--- src/main/java/fructose/FructoseApplication.java
+++ src/main/java/fructose/FructoseApplication.java
@@ -12,7 +12,14 @@ public class FructoseApplication {
 //		1. Run this file
 //		2. Go to CMD, Change Directory into front-end/fructose and run "npm start"
 //		3. Go to browser and type "localhost:3000"
-	
+
+
+//		Connexion à la BD avec console
+//		psql -U team24 -d fructose
+//		user team24
+//		password team24
+//		team 24 est SUPERUSER
+
 //		package.json a été modifié. Le proxy sur la ligne 5 est ajouté pour le future redirect
 	}
 }
