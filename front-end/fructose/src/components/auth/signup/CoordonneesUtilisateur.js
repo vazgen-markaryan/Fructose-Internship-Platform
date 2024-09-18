95e09257485eb171f4fabd95782bafed9c48c8d7
Joan Favrichon <2154540@claurendeau.qc.ca>
2024-09-18 11:20:34 -0400

    debut des erreur sous les champs des formulaire CoordoneesUtilisateur

--- front-end/fructose/src/components/auth/signup/CoordonneesUtilisateur.js
+++ front-end/fructose/src/components/auth/signup/CoordonneesUtilisateur.js
@@ -1,41 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
 const CoordonneesUtilisateur = ({utilisateur, handleChange, switchStep}) => {
 
+    const [errors, setErrors] = useState({});
+
     const handleSubmit = (event) => {
         event.preventDefault();
-
         const errorMessage = validateFields();
-        if (errorMessage) {
-            console.log(errorMessage)
-            return;
+        if (Object.keys(errorMessage).length > 0) {
+            setErrors(errorMessage);
         } else {
-            switchStep(true)
+            switchStep(true);
         }
     };
 
     const validateFields = () => {
+        let errors = {};
+
         if (!/^\(\d{3}\) \d{3}-\d{4}$/.test(utilisateur.phoneNumber)) {
-            return "Le numéro de téléphone doit être au format (123) 456-7890";
+            errors.phoneNumber = "Le numéro de téléphone doit être au format (123) 456-7890";
         }
         if (utilisateur.address.length < 3 || utilisateur.address.length > 100) {
-            return "L'adresse doit contenir entre 3 et 100 caractères";
+            errors.address = "L'adresse doit contenir entre 3 et 100 caractères";
         }
         if (!/^[A-Za-z0-9\s]+$/.test(utilisateur.address)) {
-            return "L'adresse doit contenir uniquement des lettres et des chiffres";
+            errors.address = "L'adresse doit contenir uniquement des lettres et des chiffres";
         }
+        return errors;
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label>Adresse:</label>
-                <input type="text" name="address" required value={utilisateur.address} onChange={handleChange} />
+                <input type="text" name="address" required value={utilisateur.address} onChange={handleChange}/>
+                <p style={{color: 'red'}}>{errors.address}</p>
 
                 <label>Numéro de téléphone:</label>
-                <input type="text" name="phoneNumber" required value={utilisateur.phoneNumber} onChange={handleChange} />
+                <input type="text" name="phoneNumber" required value={utilisateur.phoneNumber} onChange={handleChange}/>
+                <p style={{color: 'red'}}>{errors.phoneNumber}</p>
+
                 <br/>
-                <button onClick={() => {switchStep(false)}}>Reculer</button>
+                <button onClick={() => {
+                    switchStep(false)
+                }}>Reculer
+                </button>
                 <button type={"submit"}>Continuer</button>
             </form>
         </div>
