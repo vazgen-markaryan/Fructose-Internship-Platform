90feb8c2a855efcdb89d76eee648afba8431bff5
Joan Favrichon <2154540@claurendeau.qc.ca>
2024-09-18 10:55:15 -0400

    debut des erreur sous les champs des formulaire

--- front-end/fructose/src/components/auth/signup/InformationsBase.js
+++ front-end/fructose/src/components/auth/signup/InformationsBase.js
@@ -1,39 +1,52 @@
+import React, {useState} from "react";
 const InformationsBase = ({utilisateur, handleChange, switchStep}) => {
+
+    const [errors, setErrors] = useState({});
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const errorMessage = validateFields();
-        if (errorMessage) {
-            console.log(errorMessage)
+        if (Object.keys(errorMessage).length > 0) {
+            setErrors(errorMessage);
         } else {
-            switchStep(true)
+            switchStep(true);
         }
     };
 
     const validateFields = () => {
-        if (!/^[A-Za-z\s]+$/.test(utilisateur.fullName)) {
-            return "Le nom complet doit contenir uniquement des lettres et des espaces";
+        let errors = {};
+        if (!/^[A-Za-z\s]+$/.test(utilisateur.firstName)) {
+            errors.firstName = "Le prénom doit contenir uniquement des lettres et des espaces";
+        }
+        if (!/^[A-Za-z\s]+$/.test(utilisateur.lastName)) {
+            errors.lastName = "Le nom doit contenir uniquement des lettres et des espaces";
         }
         if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(utilisateur.email)) {
-            return "L'adresse courriel doit être valide";
+            errors.email = "L'adresse courriel doit être valide";
         }
+        return errors;
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label>Nom:</label>
-                <input type="text" name="lastName" required value={utilisateur.lastName} onChange={handleChange} />
+                <input type="text" name="lastName" required value={utilisateur.lastName} onChange={handleChange}/>
+                <p style={{color: 'red'}}>{errors.lastName}</p>
 
                 <label>Prénom:</label>
-                <input type="text" name="firstName" required value={utilisateur.firstName} onChange={handleChange} />
+                <input type="text" name="firstName" required value={utilisateur.firstName} onChange={handleChange}/>
+                <p style={{color: 'red'}}>{errors.firstName}</p>
 
                 <label>Email:</label>
-                <input type="email" name="email" required value={utilisateur.email} onChange={handleChange} />
+                <input type="text" name="email" required value={utilisateur.email} onChange={handleChange}/>
+                <p style={{color: 'red'}}>{errors.email}</p>
 
                 <br/>
-
-                <button onClick={() => {switchStep(false)}}>Reculer</button>
+                <button onClick={() => {
+                    switchStep(false)
+                }}>Reculer
+                </button>
                 <button type={"submit"}>Continuer</button>
             </form>
         </div>
