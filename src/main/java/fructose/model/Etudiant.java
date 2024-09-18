a1181df7f7788845d06948d87fb2d21516314771
Lou Jonathan Monty <2111048@claurendeau.qc.ca>
2024-09-17 20:42:28 -0400

    Ajout de offre stage et relation vers etudiant

--- src/main/java/fructose/model/Etudiant.java
+++ src/main/java/fructose/model/Etudiant.java
@@ -2,9 +2,12 @@ package fructose.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Set;
 
 @Entity
 @Setter
@@ -12,11 +15,16 @@ import lombok.ToString;
 @ToString
 @DiscriminatorValue("RECORD_ETUDIANT")
 public class Etudiant extends Utilisateur {
+    @ManyToMany(mappedBy = "etudiants")
+    private Set<OffreStage> offresStage;
+
 	public Etudiant() {
 		super();
 	}
-	
+
 	public Etudiant(String fullName, String email, String password, String matricule, String role, String departement, String companyName) {
 		super(fullName, email, password, matricule, role, departement, companyName);
 	}
+
+
 }
---
+++ src/main/java/fructose/model/OffreStage.java
@@ -0,0 +1,153 @@
+package fructose.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "offre_stage")
+public class OffreStage {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "Le nom doit contenir uniquement des lettres et des espaces")
+    private String nom;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "Le poste doit contenir uniquement des lettres et des espaces")
+    private String poste;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 10, max = 500)
+    private String description;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "La compagnie doit contenir uniquement des lettres et des espaces")
+    private String compagnie;
+
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^(Technique de l'informatique|Génie physique|Soin infirmiers)$", message = "Le programme d'étude doit être l'un des suivants : Technique de l'informatique, Génie physique, Soin infirmiers")
+    private String programmeEtude;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 10)
+    @Pattern(regexp = "^\\d+(\\.\\d{1,2})?$", message = "Le taux horaire doit être un nombre valide")
+    private String tauxHoraire;
+
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^(Présentiel|Virtuel|Hybride)$", message = "Le type d'emploi doit être l'un des suivants : Présentiel, Virtuel, Hybride")
+    private String typeEmploi;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    private String adresse;
+
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^(Temps plein|Temps partiel)$", message = "La modalité de travail doit être l'une des suivantes : Temps plein, Temps partiel")
+    private String modaliteTravail;
+
+    @NotNull
+    private LocalDate dateDebut;
+
+    @NotNull
+    private LocalDate dateFin;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 50)
+    private String dureeStage;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 50)
+    private String nombreHeuresSemaine;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 50)
+    private int nombrePostes;
+
+    @NotNull
+    private LocalDate dateLimiteCandidature;
+
+    @ManyToMany
+    @JoinTable(
+            name = "offre_stage_etudiant",
+            joinColumns = @JoinColumn(name = "offre_stage_id"),
+            inverseJoinColumns = @JoinColumn(name = "etudiant_id")
+    )
+    private Set<Etudiant> etudiants;
+
+    public static OffreStage createOffreStage(String nom,
+                                              String poste,
+                                              String description,
+                                              String compagnie,
+                                              String programmeEtude,
+                                              String tauxHoraire,
+                                              String typeEmploi,
+                                              String adresse,
+                                              String modaliteTravail,
+                                              LocalDate dateDebut,
+                                              LocalDate dateFin,
+                                              String dureeStage,
+                                              String nombreHeuresSemaine,
+                                              int nombrePostes,
+                                              LocalDate dateLimiteCandidature
+    ) {
+        OffreStage offreStage = new OffreStage();
+        offreStage.setNom(nom);
+        offreStage.setPoste(poste);
+        offreStage.setDescription(description);
+        offreStage.setCompagnie(compagnie);
+        offreStage.setProgrammeEtude(programmeEtude);
+        offreStage.setTauxHoraire(tauxHoraire);
+        offreStage.setTypeEmploi(typeEmploi);
+        offreStage.setAdresse(adresse);
+        offreStage.setModaliteTravail(modaliteTravail);
+        offreStage.setDateDebut(dateDebut);
+        offreStage.setDateFin(dateFin);
+        offreStage.setDureeStage(dureeStage);
+        offreStage.setNombreHeuresSemaine(nombreHeuresSemaine);
+        offreStage.setNombrePostes(nombrePostes);
+        offreStage.setDateLimiteCandidature(dateLimiteCandidature);
+        return offreStage;
+    }
+
+    public void addEtudiant(Etudiant etudiant) {
+        this.etudiants.add(etudiant);
+    }
+
+    public void removeEtudiant(Etudiant etudiant) {
+        this.etudiants.remove(etudiant);
+    }
+
+    public void removeAllEtudiants() {
+        this.etudiants.clear();
+    }
+
+}
