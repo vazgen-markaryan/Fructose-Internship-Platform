12acc14c6a82ee0836cf234beb7792f06eb2e392
Lou Jonathan Monty <2111048@claurendeau.qc.ca>
2024-09-17 21:38:10 -0400

    Ajout du repository OffreStageRepository

---
+++ src/main/java/fructose/repository/OffreStageRepository.java
@@ -0,0 +1,9 @@
+package fructose.repository;
+
+import fructose.model.OffreStage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OffreStageRepository extends JpaRepository<OffreStage, Long> {
+}
