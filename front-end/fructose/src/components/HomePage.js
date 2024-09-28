3093f721a7d74b150ecd2df0923082e2f71e4084
Vazgen Markaryan <vazgenmarkaryan21@gmail.com>
2024-09-28 18:47:09 -0400

    ConnexionUtilisateur est traduite

--- front-end/fructose/src/components/HomePage.js
+++ front-end/fructose/src/components/HomePage.js
@@ -12,10 +12,10 @@ const HomePage = () => {
                 <Link to="/"><img src="/assets/logo/logo.svg" alt="" className={"logo"}/></Link>
                 <div className={"toolbar-spacer"}></div>
                 <Link to="/connexion"><button style={{"backgroundColor":"transparent", "color":"white"}}><Icon path={mdiAccount} size={1} /></button></Link>
-                <Link to="/creer-utilisateur"><button style={{"fontSize":"18px"}} className={"btn-filled"}>{t("homepage.signup")}</button></Link>
+                <Link to="/creer-utilisateur"><button style={{"fontSize":"18px"}} className={"btn-filled"}>{t("home_page.signup")}</button></Link>
             </div>
             <div style={{"padding": "50px"}}>
-                <h1 style={{"fontSize":"96px", "position": "absolute", "bottom": "20%"}}>{t("homepage.carriere")}<br/>{t("homepage.commence")}<span style={{"backgroundColor": "#ff006c", "padding":"0 12px"}}>{t("homepage.ici")}.</span></h1>
+                <h1 style={{"fontSize":"96px", "position": "absolute", "bottom": "20%"}}>{t("home_page.carriere")}<br/>{t("home_page.commence")}<span style={{"backgroundColor": "#ff006c", "padding":"0 12px"}}>{t("home_page.ici")}.</span></h1>
             </div>
         </div>
     );
