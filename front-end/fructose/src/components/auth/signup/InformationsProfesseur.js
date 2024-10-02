b570176daac4af913a6386c54a12638120febf53
Vazgen Markaryan <vazgenmarkaryan21@gmail.com>
2024-10-02 18:57:05 -0400

    Refactoring

--- front-end/fructose/src/components/auth/signup/InformationsProfesseur.js
+++ front-end/fructose/src/components/auth/signup/InformationsProfesseur.js
@@ -78,41 +78,44 @@ const InformationsProfesseur = ({utilisateur, handleChange, switchStep}) => {
                 </div>
                 <div className={"input-container"}>
                     <p>{t("information_professeur_page.department")}:</p>
-                    <select name="departement" className={`${errors.departement ? "field-invalid" : ""}`} onChange={handleInputChange} value={utilisateur.departement || ""} required>
-                        <option value="">{t("information_professeur_page.select_department")}</option>
+                    <select name="departement" className={`${errors.departement ? "field-invalid" : ""}`}
+                            onChange={handleInputChange} value={utilisateur.departement || ""} required>
                         <option value="">{t("information_etudiant_page.select_program")}</option>
-                        <option value="Cinéma">{t("information_etudiant_page.programme.cinema")}</option>
-                        <option value="Gestion de commerces">{t("information_etudiant_page.programme.gestion_commerce")}</option>
-                        <option value="Gestion des opérations et de la chaîne logistique">{t("information_etudiant_page.programme.gestion_operations_chaine_logistique")}</option>
-                        <option value="Journalisme multimédia">{t("information_etudiant_page.programme.journalisme_multimedia")}</option>
-                        <option value="Langues – profil Trilinguisme et cultures">{t("information_etudiant_page.programme.langues_trilinguisme_cultures")}</option>
-                        <option value="Photographie et design graphique">{t("information_etudiant_page.programme.photographie_design_graphique")}</option>
-                        <option value="Sciences de la nature">{t("information_etudiant_page.programme.sciences_nature")}</option>
-                        <option value="Sciences humaines – profil Administration et économie">{t("information_etudiant_page.programme.sciences_humaines_administration_economie")}</option>
-                        <option value="Sciences humaines – profil Individu et relations humaines">{t("information_etudiant_page.programme.sciences_humaines_individu_relations_humaines")}</option>
-                        <option value="Sciences humaines – profil Monde en action">{t("information_etudiant_page.programme.sciences_humaines_monde_en_action")}</option>
-                        <option value="Soins infirmiers">{t("information_etudiant_page.programme.soins_infirmiers")}</option>
-                        <option value="Soins infirmiers pour auxiliaires">{t("information_etudiant_page.programme.soins_infirmiers_auxiliaires")}</option>
-                        <option value="Techniques d’éducation à l’enfance">{t("information_etudiant_page.programme.techniques_education_enfance")}</option>
-                        <option value="Techniques de bureautique">{t("information_etudiant_page.programme.techniques_bureautique")}</option>
-                        <option value="Techniques de comptabilité et de gestion">{t("information_etudiant_page.programme.techniques_comptabilite_gestion")}</option>
-                        <option value="Techniques de l’informatique">{t("information_etudiant_page.programme.techniques_informatique")}</option>
-                        <option value="Techniques de travail social">{t("information_etudiant_page.programme.techniques_travail_social")}</option>
-                        <option value="Technologie de l’architecture">{t("information_etudiant_page.programme.technologie_architecture")}</option>
-                        <option value="Technologie de l’estimation et de l’évaluation en bâtiment">{t("information_etudiant_page.programme.technologie_estimation_evaluation_batiment")}</option>
-                        <option value="Technologie du génie civil">{t("information_etudiant_page.programme.technologie_genie_civil")}</option>
-                        <option value="Technologie du génie électrique: automatisation et contrôle">{t("information_etudiant_page.programme.technologie_genie_electrique")}</option>
-                        <option value="Technologie du génie physique">{t("information_etudiant_page.programme.technologie_genie_physique")}</option>
-                        <option value="Tremplin DEC">{t("information_etudiant_page.programme.tremplin_dec")}</option>
+                        <option value="cinema">{t("information_etudiant_page.programme.cinema")}</option>
+                        <option value="gestion_commerce">{t("information_etudiant_page.programme.gestion_commerce")}</option>
+                        <option value="gestion_operations_chaine_logistique">{t("information_etudiant_page.programme.gestion_operations_chaine_logistique")}</option>
+                        <option value="journalisme_multimedia">{t("information_etudiant_page.programme.journalisme_multimedia")}</option>
+                        <option value="langues_trilinguisme_cultures">{t("information_etudiant_page.programme.langues_trilinguisme_cultures")}</option>
+                        <option value="photographie_design_graphique">{t("information_etudiant_page.programme.photographie_design_graphique")}</option>
+                        <option value="sciences_nature">{t("information_etudiant_page.programme.sciences_nature")}</option>
+                        <option value="sciences_humaines_administration_economie">{t("information_etudiant_page.programme.sciences_humaines_administration_economie")}</option>
+                        <option value="sciences_humaines_individu_relations_humaines">{t("information_etudiant_page.programme.sciences_humaines_individu_relations_humaines")}</option>
+                        <option value="sciences_humaines_monde_en_action">{t("information_etudiant_page.programme.sciences_humaines_monde_en_action")}</option>
+                        <option value="soins_infirmiers">{t("information_etudiant_page.programme.soins_infirmiers")}</option>
+                        <option value="soins_infirmiers_auxiliaires">{t("information_etudiant_page.programme.soins_infirmiers_auxiliaires")}</option>
+                        <option value="techniques_education_enfance">{t("information_etudiant_page.programme.techniques_education_enfance")}</option>
+                        <option value="techniques_bureautique">{t("information_etudiant_page.programme.techniques_bureautique")}</option>
+                        <option value="techniques_comptabilite_gestion">{t("information_etudiant_page.programme.techniques_comptabilite_gestion")}</option>
+                        <option value="techniques_informatique">{t("information_etudiant_page.programme.techniques_informatique")}</option>
+                        <option value="techniques_travail_social">{t("information_etudiant_page.programme.techniques_travail_social")}</option>
+                        <option value="technologie_architecture">{t("information_etudiant_page.programme.technologie_architecture")}</option>
+                        <option value="technologie_estimation_evaluation_batiment">{t("information_etudiant_page.programme.technologie_estimation_evaluation_batiment")}</option>
+                        <option value="technologie_genie_civil">{t("information_etudiant_page.programme.technologie_genie_civil")}</option>
+                        <option value="technologie_genie_electrique">{t("information_etudiant_page.programme.technologie_genie_electrique")}</option>
+                        <option value="technologie_genie_physique">{t("information_etudiant_page.programme.technologie_genie_physique")}</option>
+                        <option value="tremplin_dec">{t("information_etudiant_page.programme.tremplin_dec")}</option>
                     </select>
                     <p className={"field-invalid-text"}>{errors.departement}</p>
                 </div>
                 <br/>
 
                 <div className="form-dock">
-                    <button onClick={() => {switchStep(false)}}><Icon path={mdiChevronLeft} size={1}/></button>
+                    <button onClick={() => {
+                        switchStep(false)
+                    }}><Icon path={mdiChevronLeft} size={1}/></button>
                     <div className={"toolbar-spacer"}></div>
-                    <button type="submit" className={"btn-filled"}>{t("information_professeur_page.continue")}<Icon path={mdiChevronRight} size={1}/></button>
+                    <button type="submit" className={"btn-filled"}>{t("information_professeur_page.continue")}<Icon
+                        path={mdiChevronRight} size={1}/></button>
                 </div>
             </form>
         </div>
