5e41f8a57d4a5c190f01454a14d3087ae508b814
Vazgen Markaryan <vazgenmarkaryan21@gmail.com>
2024-10-09 10:14:00 -0400

    Traduction HeaderMain

--- front-end/fructose/src/components/fragments/header/HeaderMain.js
+++ front-end/fructose/src/components/fragments/header/HeaderMain.js
@@ -3,13 +3,14 @@ import Icon from "@mdi/react";
 import {mdiAccount, mdiMessage, mdiMessageOutline, mdiViewDashboard, mdiViewDashboardOutline} from "@mdi/js";
 import React, {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../../../providers/AuthProvider";
+import {useTranslation} from "react-i18next";
 
 const HeaderMain = ({theme}) => {
+
+    const {t} = useTranslation();
     const [menuOpen, setMenuOpen] = useState(false)
     const [notificationMenuOpen, setNotificationMenuOpen] = useState(false)
-
-    const { currentUser, SignOutUser } = useContext(AuthContext);
-
+    const {currentUser, SignOutUser} = useContext(AuthContext);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
@@ -31,35 +32,47 @@ const HeaderMain = ({theme}) => {
     }
 
     const GetHeaderOptions = () => {
-        if(currentUser == null){
+        if (currentUser == null) {
             return (
                 <>
-                    <Link to="/connexion"><button style={{"backgroundColor":"transparent", "color":"inherit"}}><Icon path={mdiAccount} size={1} /></button></Link>
-                    <Link to="/creer-utilisateur"><button style={{"fontSize":"18px"}} className={"btn-filled"}>S'inscrire</button></Link>
+                    <Link to="/connexion">
+                        <button style={{"backgroundColor": "transparent", "color": "inherit"}}><Icon path={mdiAccount} size={1}/></button>
+                    </Link>
+                    <Link to="/creer-utilisateur">
+                        <button style={{"fontSize": "18px"}} className={"btn-filled"}>{t("home_page.signup")}</button>
+                    </Link>
                 </>
-                )
+            )
         } else {
             return (
                 <>
-                    <div onClick={() => {closeAllMenus()}} className={"header-user-menu-obstruct"} style={{"display": (isMenuOpen())?"block":"none"}}></div>
+                    <div onClick={() => {closeAllMenus()}} className={"header-user-menu-obstruct"} style={{"display": (isMenuOpen()) ? "block" : "none"}}></div>
 
-                    <Link to="/dashboard"><button style={{"backgroundColor":"transparent", "color":"inherit", "padding": "0"}}><Icon path={mdiViewDashboardOutline} size={1} /></button></Link>
-                    <button onClick={() => {toggleNotificationMenu()}} style={{"backgroundColor":"transparent", "color":"inherit", "padding": "0 16px"}}><Icon path={mdiMessageOutline} size={1} /></button>
+                    <Link to="/dashboard">
+                        <button style={{"backgroundColor": "transparent", "color": "inherit", "padding": "0"}}><Icon path={mdiViewDashboardOutline} size={1}/></button>
+                    </Link>
+                    <button onClick={() => {toggleNotificationMenu()}} style={{"backgroundColor": "transparent", "color": "inherit", "padding": "0 16px"}}><Icon path={mdiMessageOutline} size={1}/></button>
 
-                    <button onClick={() => {toggleMenu()}} style={{"backgroundColor":"black", "color":"white", "background": "url('/assets/auth/default-profile.jpg') center / cover", "width": "42px", "height": "42px", "borderRadius": "50%"}}></button>
-                    <div className={"header-user-menu"} style={{"display": (menuOpen)?"block":"none"}}>
+                    <button onClick={() => {toggleMenu()}} style={{
+                        "backgroundColor": "black",
+                        "color": "white",
+                        "background": "url('/assets/auth/default-profile.jpg') center / cover",
+                        "width": "42px",
+                        "height": "42px",
+                        "borderRadius": "50%"
+                    }}></button>
+
+                    <div className={"header-user-menu"} style={{"display": (menuOpen) ? "block" : "none"}}>
                         <div className={"header-user-menu-profile"}>
                             <h6>{currentUser.fullName}</h6>
                             <p className={"text-dark"}>{currentUser.email}</p>
                         </div>
 
-                        <button onClick={()=>{SignOutUser()}}>
-                            Deconnexion
-                        </button>
+                        <button onClick={() => {SignOutUser()}}>{t("header_main_page.disconnect")}</button>
                     </div>
-                    <div className={"header-user-menu"} style={{"display": (notificationMenuOpen)?"block":"none"}}>
+                    <div className={"header-user-menu"} style={{"display": (notificationMenuOpen) ? "block" : "none"}}>
                         <div className={"header-user-menu-profile"}>
-                            <h6><Icon path={mdiMessage} size={1} /> Notifications</h6>
+                            <h6><Icon path={mdiMessage} size={1}/>{t("header_main_page.notifications")}</h6>
                         </div>
                     </div>
                 </>
@@ -67,13 +80,10 @@ const HeaderMain = ({theme}) => {
         }
     }
 
-    return(
-        <header style={{"color":((theme === "dark")?"black":"white")}}>
-                <Link to="/"><img src={"/assets/logo/logo" + ((theme === "dark") ? "-blk": "") + ".png"} alt="" className={"logo"}/></Link>
-                <div className={"toolbar-spacer"}></div>
-            {
-                GetHeaderOptions()
-            }
+    return (
+        <header style={{"color": ((theme === "dark") ? "black" : "white")}}>
+            <Link to="/"><img src={"/assets/logo/logo" + ((theme === "dark") ? "-blk" : "") + ".png"} alt="" className={"logo"}/></Link>
+            <div className={"toolbar-spacer"}></div>{GetHeaderOptions()}
         </header>
     )
 }
