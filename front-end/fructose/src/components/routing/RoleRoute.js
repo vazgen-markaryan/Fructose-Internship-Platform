1409f60834d8d2855568020318ad60cddb29f4a7
Vazgen Markaryan <vazgenmarkaryan21@gmail.com>
2024-10-21 15:07:05 -0400

    Refactoring du Main. Populer la BD avec some DATA

--- front-end/fructose/src/components/routing/RoleRoute.js
+++ front-end/fructose/src/components/routing/RoleRoute.js
@@ -3,9 +3,21 @@ import {Navigate} from 'react-router-dom';
 import {AuthContext} from "../../providers/AuthProvider";
 
 const RoleRoute = ({element: Component, roles}) => {
-    const {isSignedIn, currentUser} = useContext(AuthContext);
-    if (!isSignedIn() || !currentUser) {return <Navigate to="/dashboard"/>;}
-    return roles.includes(currentUser.role) ? Component : <Navigate to="/dashboard"/>;
+    const {isSignedIn, currentUser, isUserInit} = useContext(AuthContext);
+
+    if (!isUserInit) {
+        return (
+            <div className="loader-container">
+                <div className="loader"></div>
+            </div>
+        );
+    } else {
+        if (!isSignedIn() || !currentUser) {
+            return <Navigate to="/dashboard"/>;
+        }
+        return roles.includes(currentUser.role) ? Component : <Navigate to="/dashboard"/>;
+    }
+
 };
 
 export {RoleRoute};
