0b556c89c621bf652220afeb6ce3f799916dd1f0
Alex <6204624@claurendeau.qc.ca>
2024-11-05 20:05:43 -0500

    Ajout du selecteur de cv

--- front-end/fructose/src/components/pages/dashboard/offre-stage/ApplyOffreWindow.js
+++ front-end/fructose/src/components/pages/dashboard/offre-stage/ApplyOffreWindow.js
@@ -1,8 +1,33 @@
-import {mdiClose} from "@mdi/js";
+import {mdiAlert, mdiClose, mdiFileOutline, mdiMapMarkerOutline, mdiOpenInNew} from "@mdi/js";
 import Icon from "@mdi/react";
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import {AuthContext} from "../../../../providers/AuthProvider";
+import {CvContext} from "../../../../providers/CvProvider";
+import {Link} from "react-router-dom";
 
-const ApplyOffreWindow = () => {
+const ApplyOffreWindow = ({currentOffer}) => {
+    const {isUserInit} = useContext(AuthContext);
+    const {GetCvs} = useContext(CvContext);
+
+    const [approvedCvs, setApprovedCvs] = useState(null)
+
+    useEffect(() => {
+        if (isUserInit) {
+            (async function () {
+                const response = await GetCvs();
+                const data = await response.text();
+                let cvs = JSON.parse(data);
+                let approvedCvs = []
+                for (let i = 0; i < cvs.length; i++){
+                    if(cvs[i].isApproved === true && cvs[i].isRefused === false){
+                        approvedCvs.push(cvs[i])
+                    }
+                }
+                setApprovedCvs(approvedCvs)
+            })();
+        }
+    }, [isUserInit, GetCvs]);
+
     return (
         <div className="window-frame">
             <div className="window">
@@ -11,30 +36,57 @@ const ApplyOffreWindow = () => {
                     <button className="btn-icon"><Icon path={mdiClose} size={1}/></button>
                 </div>
                 <div className="window-content">
-                    <div className="toolbar-items" style={{gap: "8px"}}>
-                        <div className="user-profile-section-profile-picture" style={{
-                            "background": "url('/assets/offers/default-company.png') center / cover",
-                            width: "32px",
-                            height: "32px",
-                            borderRadius: "5px",
-                            margin: 0
-                        }}></div>
-                        <div>
-                            <h6 className="m-0">Faire je ne sais quoi</h6>
-                            <p className="m-0 text-dark">Ubisoft de quoi</p>
+                    <section className="nospace">
+                        <div className="toolbar-items" style={{gap: "8px"}}>
+                            <div className="user-profile-section-profile-picture" style={{
+                                "background": "url('/assets/offers/default-company.png') center / cover",
+                                width: "52px",
+                                height: "52px",
+                                borderRadius: "5px",
+                                margin: 0
+                            }}></div>
+                            <div>
+                                <h4 className="m-0">Faire je ne sais quoi</h4>
+                                <h6 className="m-0 text-dark">Ubisoft de quoi</h6>
+                            </div>
                         </div>
-                    </div>
-                    <br/>
-                    <h6 className="m-0">Curriculum Vitae à envoyer</h6>
-                    <select name="" id="">
-                        <option value="">cv1.pdf</option>
-                    </select>
-                    <p className="text-dark">Note: Seuls les CV approuvés apparaîtront dans la liste.</p>
+                    </section>
+                    <section className="nospace">
+                        <h5></h5>
+                        <div className="list-bullet">
+                            <Icon path={mdiMapMarkerOutline} size={1}/>
+                            <div style={{padding: "4px 0"}}>
+                                <h6 className="m-0" style={{marginBottom: "5px"}}>123 Street Street </h6>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="nospace">
+                        <br/>
+                        <h6 className="m-0">Curriculum Vitae à envoyer</h6>
+                        {
+                            (approvedCvs !== null)?
+                                (approvedCvs.length !== 0)
+                                    ?
+                                    <>
+                                        <select name="cv" id="">
+                                            {approvedCvs.reverse().map((item, index) => (
+                                                <option key={index} value={item.id}>{item.filename}</option>
+                                            ))}
+                                        </select>
+                                        <p className="text-dark">Note: Seuls les CV approuvés apparaîtront dans la liste.</p>
+                                    </>
+                                    :
+                                    <div className="banner"><Icon path={mdiAlert} size={1} /><p>Aucun CV approuvé dans votre dossier. Vous ne pourrez postuler sans un CV approuvé. <Link to="/dashboard/manage-cvs">Voir mes CVs <Icon path={mdiOpenInNew} size={0.5} /></Link></p></div>
+                                :
+                                <div className="loader"></div>
+                        }
+
+                    </section>
                 </div>
                 <div className="window-options">
                     <div className="toolbar-spacer"></div>
                     <button>Annuler</button>
-                    <button className="btn-filled">Postuler</button>
+                    <button className="btn-filled" disabled={(approvedCvs !== null)?(approvedCvs.length === 0):true}>Postuler</button>
                 </div>
             </div>
         </div>
