62eaeb2dfa50540702ae92361807a82d3e75beef
Lou Jonathan Monty <2111048@claurendeau.qc.ca>
2024-11-16 14:28:50 -0500

    ÃAjout d'un contrat service basique

---
+++ src/main/java/fructose/service/ContratService.java
@@ -0,0 +1,80 @@
+package fructose.service;
+
+import fructose.model.Contrat;
+import fructose.model.Utilisateur;
+import fructose.model.enumerator.Role;
+import fructose.repository.ContratRepository;
+import fructose.repository.UtilisateurRepository;
+import fructose.service.dto.ContratDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ContratService {
+
+    private final ContratRepository contratRepository;
+    private final UtilisateurRepository utilisateurRepository;
+
+    public Utilisateur getUtilisateurEnCours() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return utilisateurRepository.findByEmail(authentication.getName());
+        }
+        throw new IllegalArgumentException("Aucun utilisateur n'est connecté");
+    }
+
+
+    public ContratDTO createContrat(ContratDTO contratDTO) {
+        Contrat contrat = ContratDTO.toEntity(contratDTO);
+        contrat = contratRepository.save(contrat);
+        return ContratDTO.toDTO(contrat);
+    }
+
+    public void deleteContrat(Long id) {
+        if (!contratRepository.existsById(id)) {
+            throw new IllegalArgumentException("Contrat with ID: " + id + " does not exist");
+        }
+        contratRepository.deleteById(id);
+    }
+
+    public ContratDTO getContratById(Long id) {
+        Contrat contrat = contratRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Contrat with ID: " + id + " does not exist"));
+        return ContratDTO.toDTO(contrat);
+    }
+
+    public List<ContratDTO> getAllContrats() {
+        return contratRepository.findAll().stream()
+                .map(ContratDTO::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ContratDTO signContrat(Long id, String signature) {
+        Contrat contrat = contratRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Contrat with ID: " + id + " does not exist"));
+
+        Utilisateur utilisateur = getUtilisateurEnCours();
+        Role role = utilisateur.getRole();
+        switch (role) {
+            case EMPLOYEUR:
+                contrat.setSignatureEmployeur(signature);
+                break;
+            case ETUDIANT:
+                contrat.setSignatureEtudiant(signature);
+                break;
+            case ADMIN:
+                contrat.setSignatureGestionnaire(signature);
+                break;
+            default:
+                throw new IllegalArgumentException("Role not supported");
+        }
+        contrat = contratRepository.save(contrat);
+        return ContratDTO.toDTO(contrat);
+    }
+}
