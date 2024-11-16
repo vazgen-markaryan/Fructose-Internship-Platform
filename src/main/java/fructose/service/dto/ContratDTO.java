d1d4d5f50c10e6b5765e69adbb21b9529329d7ce
Lou Jonathan Monty <2111048@claurendeau.qc.ca>
2024-11-16 16:14:48 -0500

    Modification du DTO pour ajouter les dates de signature

--- src/main/java/fructose/service/dto/ContratDTO.java
+++ src/main/java/fructose/service/dto/ContratDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.LocalDate;
 
 
 @Data
@@ -26,11 +27,17 @@ public class ContratDTO {
     @NotBlank
     private String signatureGestionnaire;
 
+    private LocalDate dateSignatureGestionnaire;
+
     @NotBlank
     private String signatureEmployeur;
 
+    private LocalDate dateSignatureEmployeur;
+
     @NotBlank
     private String signatureEtudiant;
+
+    private LocalDate dateSignatureEtudiant;
 
     public static ContratDTO toDTO(Contrat contrat) {
         ContratDTO contratDTO = new ContratDTO();
@@ -40,8 +47,11 @@ public class ContratDTO {
         contratDTO.setEtudiant(EtudiantDTO.toDTO(contrat.getEtudiant()));
         contratDTO.setOffreStage(OffreStageDTO.toDTO(contrat.getOffreStage()));
         contratDTO.setSignatureGestionnaire(contrat.getSignatureGestionnaire());
+        contratDTO.setDateSignatureGestionnaire(contrat.getDateSignatureGestionnaire());
         contratDTO.setSignatureEmployeur(contrat.getSignatureEmployeur());
+        contratDTO.setDateSignatureEmployeur(contrat.getDateSignatureEmployeur());
         contratDTO.setSignatureEtudiant(contrat.getSignatureEtudiant());
+        contratDTO.setDateSignatureEtudiant(contrat.getDateSignatureEtudiant());
         return contratDTO;
     }
 
@@ -53,8 +63,11 @@ public class ContratDTO {
         contrat.setEtudiant(EtudiantDTO.toEntity(contratDTO.getEtudiant()));
         contrat.setOffreStage(OffreStageDTO.toEntity(contratDTO.getOffreStage()));
         contrat.setSignatureGestionnaire(contratDTO.getSignatureGestionnaire());
+        contrat.setDateSignatureGestionnaire(contratDTO.getDateSignatureGestionnaire());
         contrat.setSignatureEmployeur(contratDTO.getSignatureEmployeur());
+        contrat.setDateSignatureEmployeur(contratDTO.getDateSignatureEmployeur());
         contrat.setSignatureEtudiant(contratDTO.getSignatureEtudiant());
+        contrat.setDateSignatureEtudiant(contratDTO.getDateSignatureEtudiant());
         return contrat;
     }
 }
