889cffaba9dbe99ff550dd4c5717f2141e774771
Alex <6204624@claurendeau.qc.ca>
2024-11-19 23:45:45 -0500

    Tests réglés dans Modifier Offre Stage

--- front-end/fructose/src/components/pages/offre-stage/__test__/ModifierOffreStage.test.js
+++ front-end/fructose/src/components/pages/offre-stage/__test__/ModifierOffreStage.test.js
@@ -27,17 +27,21 @@ afterEach(() => {
 });
 
 describe('ModifierOffreStage Component', () => {
-    test('Renders without errors', () => {
+    test('Renders without errors', async () => {
+        const mockOffer = JSON.parse("{\"id\":32,\"nom\":\"aaaaaa\",\"poste\":\"Scrum Master\",\"description\":\"dsdsdsdsadas\",\"compagnie\":\"Alex Inc.\",\"departementDTO\":{\"id\":17,\"nom\":\"techniques_informatique\"},\"tauxHoraire\":0.0,\"typeEmploi\":\"presentiel\",\"adresse\":\"6666 Rue du diable H6A 6N6\",\"modaliteTravail\":\"temps_plein\",\"dateDebut\":\"2024-11-29\",\"dateFin\":\"2024-12-07\",\"nombreHeuresSemaine\":1,\"nombrePostes\":7,\"dateLimiteCandidature\":\"2024-11-28\",\"ownerDTO\":{\"id\":13,\"fullName\":\"Yves Guillemot\",\"email\":\"ubisoft@gmail.com\",\"password\":\"$2a$10$cGX2U0jw1xzHFOys9c.Hl.BXgm3XUAjFT9xLlEvuFN1KP3S1sH4xC\",\"matricule\":null,\"role\":\"EMPLOYEUR\",\"departementDTO\":{\"id\":17,\"nom\":\"techniques_informatique\"},\"companyName\":\"Ubisoft Incorporé\",\"isApproved\":true},\"isApproved\":false,\"isRefused\":false,\"commentaireRefus\":\"Commentaire par défaut\"}")
+        mockFetchOffreStage.mockResolvedValue(mockOffer);
         render(
             <AuthContext.Provider value={mockAuthContext}>
-                <OffreStageContext.Provider value={{}}>
+                <OffreStageContext.Provider value={{fetchOffreStage: mockFetchOffreStage}}>
                     <Router>
-                        <ModifierOffreStage />
+                        <ModifierOffreStage/>
                     </Router>
                 </OffreStageContext.Provider>
             </AuthContext.Provider>
         );
-        expect(screen.getByText(/modifier_offre_stage_page.title/i)).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText(/modifier_offre_stage_page.title/i)).toBeInTheDocument();
+        });
     });
 
     test('Returns 404 error when not found', async () => {
@@ -55,6 +59,9 @@ describe('ModifierOffreStage Component', () => {
             </AuthContext.Provider>
         );
         // Then
-        expect(screen.getByText(/Erreur 404/i)).toBeInTheDocument();
+
+        await waitFor(() => {
+            expect(screen.getByText(/Erreur 404/i)).toBeInTheDocument();
+        });
     });
 })
