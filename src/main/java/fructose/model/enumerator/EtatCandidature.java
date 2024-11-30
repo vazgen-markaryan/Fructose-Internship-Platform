b9750224e704d6c3b585d1e0493aff85adc195e6
Vazgen Markaryan <vazgenmarkaryan21@gmail.com>
2024-11-30 15:17:28 -0500

    Backend oublie lol

--- src/main/java/fructose/model/enumerator/EtatCandidature.java
+++ src/main/java/fructose/model/enumerator/EtatCandidature.java
@@ -17,5 +17,6 @@ public enum EtatCandidature {
 	REFUSEE_APRES_ENTREVUE,                         // Offre refusée par l'étudiant après entrevue
 	
 	// Phase de signature de contrat
-	CONTRAT_CREE_PAR_GESTIONNAIRE       // Contrat créé par le gestionnaire
+	CONTRAT_CREE_PAR_GESTIONNAIRE,      // Contrat créé par le gestionnaire
+	POSTE_OBTENU          				          // Contrat signé par tous les parties
 }
